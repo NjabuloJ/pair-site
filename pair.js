5de277892c51f3545b60e9e101fd229567e0306f
NjabuloJ <fanajb65@gmail.com>
2025-06-11 00:55:13 +0200

    Update pair.js

--- pair.js
+++ pair.js
@@ -1,4 +1,5 @@
-const PastebinAPI = require('pastebin-js'), pastebin = new PastebinAPI('EMWTMkQAVfJa9kM-MRUrxd5Oku1U7pgL')
+const PastebinAPI = require('pastebin-js'), 
+pastebin = new PastebinAPI('EMWTMkQAVfJa9kM-MRUrxd5Oku1U7pgL') 
 const {makeid} = require('./id'); 
 const express = require('express'); 
 const fs = require('fs'); 
@@ -37,8 +38,8 @@ router.get('/', async (req, res) => {
                 } 
             } 
 
-            Pair_Code_By_Gifted_Tech.ev.on('creds.update', saveCreds) 
-const PastebinAPI = require('pastebin-js'), pastebin = new PastebinAPI('EMWTMkQAVfJa9kM-MRUrxd5Oku1U7pgL')
+const PastebinAPI = require('pastebin-js'), 
+pastebin = new PastebinAPI('EMWTMkQAVfJa9kM-MRUrxd5Oku1U7pgL') 
 const {makeid} = require('./id'); 
 const express = require('express'); 
 const fs = require('fs'); 
@@ -88,78 +89,122 @@ router.get('/', async (req, res) => {
                     let b64data = Buffer.from(data).toString('base64'); 
                     let session = await Pair_Code_By_Gifted_Tech.sendMessage(Pair_Code_By_Gifted_Tech.user.id, { text: '' + b64data }); 
 
-                    let img = "https:                                
-                    let GIFTED_MD_TEXT = `https:                                                 
-                    let buttons = [
-                        {
-                            buttonId: 'id1',
-                            buttonText: {displayText: 'Join Channel'},
-                            type: 1
+                    let img = "https:                          
+                    let GIFTED_MD_TEXT = `https:                                                  
+                    let buttons = [ 
+                        { 
+                            buttonId: 'id1', 
+                            buttonText: {displayText: 'Join Channel'}, 
+                            type: 1, 
+                            buttonParamsJson: JSON.stringify({ 
+                                display_text: 'Join Channel', 
+                                url: 'https:                                                 
+                            }) 
+                        } 
+                    ]; 
+
+                    await Pair_Code_By_Gifted_Tech.sendMessage( 
+                        Pair_Code_By_Gifted_Tech.user.id, 
+                        { 
+                            image: { url: img }, 
+                            caption: GIFTED_MD_TEXT, 
+                            buttons: buttons, 
+                            contextInfo: { 
+                                isForwarded: true, 
+                                forwardedNewsletterMessageInfo: { 
+                                    newsletterJid: "//example.com/image.jpg"; 
+                    let GIFTED_MD_TEXT = `https://whatsapp.com/channel/0029VarYP5iAInPtfQ8fRb2T`; 
+                    let buttons = [ 
+                        { 
+                            buttonId: 'id1', 
+                            buttonText: {displayText: 'Join Channel'}, 
+                            type: 1, 
+                            buttonParamsJson: JSON.stringify({ 
+                                display_text: 'Join Channel', 
+                                url: 'https://whatsapp.com/channel/0029VarYP5iAInPtfQ8fRb2T' 
+                            }) 
+                        } 
+                    ]; 
+
+                    await Pair_Code_By_Gifted_Tech.sendMessage( 
+                        Pair_Code_By_Gifted_Tech.user.id, 
+                        { 
+                            image: { url: img }, 
+                            caption: GIFTED_MD_TEXT, 
+                            buttons: buttons, 
+                            contextInfo: { 
+                                isForwarded: true, 
+                                forwardedNewsletterMessageInfo: { 
+                                    newsletterJid: "120363345407274799@newsletter", 
+                                    newsletterName: "╭••➤®Njabulo Jb", 
+                                    serverMessageId: -1 
+                                }, 
+                                forwardingScore: 999, 
+                                externalAdReply: { 
+                                    title: "this season ld work on Njabulo Jb and Alec Jb", 
+                                    body: "session working on heroku", 
+                                    thumbnailUrl: img, 
+                                    sourceUrl: "https://whatsapp.com/channel/0029VarYP5iAInPtfQ8fRb2T", 
+                                    mediaType: 1, 
+                                    renderLargerThumbnail: true 
+                                }, 
+                            }, 
                         }
-                    ];
-
-                    await Pair_Code_By_Gifted_Tech.sendMessage(
-                        Pair_Code_By_Gifted_Tech.user.id,
-                        {
-                            image: { url: img },
-                            caption: GIFTED_MD_TEXT,
-                            buttons: buttons,
-                            contextInfo: {
-                                isForwarded: true,
-                                forwardedNewsletterMessageInfo: {
-                                    newsletterJid: "//files.catbox.moe/cvd9sb.jpg"; 
-                    let GIFTED_MD_TEXT = `https://whatsapp.com/channel/0029VarYP5iAInPtfQ8fRb2T` 
-                    let buttons = [
-                        {
-                            buttonId: 'id1',
-                            buttonText: {displayText: 'Join Channel'},
-                            type: 1
-                        }
-                    ];
-
-                    await Pair_Code_By_Gifted_Tech.sendMessage(
-                        Pair_Code_By_Gifted_Tech.user.id,
-                        {
-                            image: { url: img },
-                            caption: GIFTED_MD_TEXT,
-                            buttons: buttons,
-                            contextInfo: {
-                                isForwarded: true,
-                                forwardedNewsletterMessageInfo: {
-                                    newsletterJid: "120363345407274799@newsletter",
-                                    newsletterName: "╭••➤®Njabulo Jb",
-                                    serverMessageId: -1
-                                },
-                                forwardingScore: 999,
-                                externalAdReply: {
-                                    title: "this season ld work on Njabulo Jb and Alec Jb",
-                                    body: "session working on heroku",
-                                    thumbnailUrl: img,
-                                    sourceUrl: "https://whatsapp.com/channel/0029VarYP5iAInPtfQ8fRb2T",
-                                    mediaType: 1,
-                                    renderLargerThumbnail: true
-                                },
-                            },
-                        },
-                        { quoted: session }
-                    );
-
-                    await delay(100); 
-                    await Pair_Code_By_Gifted_Tech.ws.close(); 
-                    return await removeFile('./temp/'+id); 
-                } else if (connection === "close" && lastDisconnect && lastDisconnect.error && lastDisconnect.error.output.statusCode != 401) { 
-                    await delay(10000); 
-                    GIFTED_MD_PAIR_CODE(); 
+                    ); 
                 } 
             }); 
-        } catch (err) { 
-            console.log("service restated"); 
-            await removeFile('./temp/'+id); 
+        } catch (error) { 
+            console.error("Error in GIFTED_MD_PAIR_CODE:", error); 
             if(!res.headersSent){ 
-           await res.send({code:"Service Unavailable"});
-         }
-        }
-    }
-    return await GIFTED_MD_PAIR_CODE()
-});
-module.exports = router
+                res.status(500).send({ error: "Internal￼Enter            Pair_Code_By_Gifted_Tech.ev.on('creds.update', saveCreds) 
+
+            Pair_Code_By_Gifted_Tech.ev.on("connection.update", async (s) => { 
+                const { connection, lastDisconnect } = s; 
+                if (connection == "open") { 
+                    await delay(5000); 
+                    let data = fs.readFileSync(__dirname + `/temp/${id}/creds.json`); 
+                    await delay(800); 
+                    let b64data = Buffer.from(data).toString('base64'); 
+                    let session = await Pair_Code_By_Gifted_Tech.sendMessage(Pair_Code_By_Gifted_Tech.user.id, { text: '' + b64data }); 
+
+                    let img = "https:                          
+                    let GIFTED_MD_TEXT = `https:                                                  
+                    let buttons = [ 
+                        { 
+                            buttonId: 'id1', 
+                            buttonText: {displayText: 'Join Channel'}, 
+                            type: 1, 
+                            buttonParamsJson: JSON.stringify({ 
+                                display_text: 'Join Channel', 
+                                url: 'https:                                                 
+                            }) 
+                        } 
+                    ]; 
+
+                    await Pair_Code_By_Gifted_Tech.sendMessage( 
+                  Pair_Code_By_Gifted_Tech.user.id, 
+                        { 
+                            image: { url: img }, 
+                            caption: GIFTED_MD_TEXT, 
+                            buttons: buttons, 
+                            contextInfo: { 
+                                isForwarded: true, 
+                                forwardedNewsletterMessageInfo: { 
+                                    newsletterJid: "//example.com/image.jpg"; 
+                    let GIFTED_MD_TEXT = `https://whatsapp.com/channel/0029VarYP5iAInPtfQ8fRb2T`; 
+                    let buttons = [ 
+                        { 
+                            buttonId: 'id1', 
+                            buttonText: {displayText: 'Join Channel'}, 
+                            type: 1, 
+                            buttonParamsJson: JSON.stringify({ 
+                                display_text: 'Join Channel', 
+                                url: 'https://whatsapp.com/channel/0029VarYP5iAInPtfQ8fRb2T' 
+                            }) 
+                        } 
+                    ]; 
+
+                    await Pair_Code_By_Gifted_Tech.sendMessage( 
+                        Pair_Code_By_Gifted_Tech.user.id, 
+                        { 
+                            image: { url: img }, 
